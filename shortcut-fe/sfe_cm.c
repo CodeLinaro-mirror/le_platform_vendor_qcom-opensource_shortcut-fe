18d9dc74977da15209aed80f7451d3531d33c7fb
Aman Gupta <amangupt@codeaurora.org>
2020-09-07 15:01:08 +0530

    shortcut-fe: Changes to make sfe module compatible to kernel upgrade 5.4
    This change is to make sfe compatible to kernel - 5.4.
    These changes will help solve compilation issues on sfe.
    
    Signed-off-by: Aman Gupta <amangupt@codeaurora.org>
    
    Change-Id: Ic0186bfc2cce26b2ac7818c0d705531d81222a77

--- shortcut-fe/sfe_cm.c
+++ shortcut-fe/sfe_cm.c
@@ -315,7 +315,9 @@ static bool sfe_cm_find_dev_and_mac_addr(sfe_ip_addr_t *addr,
 
 		dst = (struct dst_entry *)rt;
 	} else {
-		rt6 = rt6_lookup(&init_net, (struct in6_addr *)addr->ip6, 0, 0, 0);
+		rt6 = rt6_lookup(&init_net,
+				(struct in6_addr *)addr->ip6,
+				0, 0, 0, 0);
 		if (!rt6) {
 			goto ret_fail;
 		}
