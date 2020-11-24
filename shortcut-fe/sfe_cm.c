75edf604b4ba7c08841aece66cae340bb2afe4a0
Aman Gupta <amangupt@codeaurora.org>
2020-11-24 16:30:12 +0530

    shortcut-fe: rt6 fix for kernel upgrade 5.4
    This change is for rt6 lookup change work only on 5.4
    These changes will help solve compilation issues on sfe.
    
    Change-Id: Id0fd8260ad1858c46c6984f49240ec8c2275dc9f
    Signed-off-by: Aman Gupta <amangupt@codeaurora.org>

--- shortcut-fe/sfe_cm.c
+++ shortcut-fe/sfe_cm.c
@@ -315,9 +315,13 @@ static bool sfe_cm_find_dev_and_mac_addr(sfe_ip_addr_t *addr,
 
 		dst = (struct dst_entry *)rt;
 	} else {
+#ifdef ISKERNEL5_4
 		rt6 = rt6_lookup(&init_net,
 				(struct in6_addr *)addr->ip6,
 				0, 0, 0, 0);
+#else
+		rt6 = rt6_lookup(&init_net, (struct in6_addr *)addr->ip6, 0, 0, 0);
+#endif
 		if (!rt6) {
 			goto ret_fail;
 		}
